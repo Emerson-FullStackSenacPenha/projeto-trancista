2b97f3707107fc70d42634b0b1583ab3b9176b6d
Emerson-FullStackSenacPenha <emerson.agomes1@senacsp.edu.br>
2025-11-14 18:41:34 -0300

    organizando dialog de serviços

--- js/servicos.js
+++ js/servicos.js
@@ -1,48 +1,49 @@
 'use strict'
 
+// Tranças
 const btTranca = document.querySelector("#btTranca");
-const btFtranca = document.querySelector("#btFtranca");
+const fecharTranca = document.querySelector("#fecharTranca");
 const tranca = document.querySelector("#trancas");
+
+btTranca.addEventListener("click", () => tranca.showModal());
+fecharTranca.addEventListener("click", () => tranca.close());
+
+// Lace
 const btLace = document.querySelector("#btLace");
-const btFlace = document.querySelector("#btFlace");
+const fecharLace = document.querySelector("#fecharLace");
 const lace = document.querySelector("#lace");
-const btApliqeu = document.querySelector("#btApliqeu");
+
+btLace.addEventListener("click", () => lace.showModal());
+fecharLace.addEventListener("click", () => lace.close());
+
+// Aplique
+const btAplique = document.querySelector("#btAplique");
+const fecharAplique = document.querySelector("#fecharAplique");
 const aplique = document.querySelector("#aplique");
+
+btAplique.addEventListener("click", () => aplique.showModal());
+fecharAplique.addEventListener("click", () => aplique.close());
+
+// Manutenção
 const btManutencao = document.querySelector("#btManutencao");
+const fecharManutencao = document.querySelector("#fecharManutencao");
 const manutencao = document.querySelector("#manutencao");
+
+btManutencao.addEventListener("click", () => manutencao.showModal());
+fecharManutencao.addEventListener("click", () => manutencao.close());
+
+// Hidronutrição
 const btHidro = document.querySelector("#btHidro");
+const fecharHidro = document.querySelector("#fecharHidro");
 const hidro = document.querySelector("#hidro");
+
+btHidro.addEventListener("click", () => hidro.showModal());
+fecharHidro.addEventListener("click", () => hidro.close());
+
+// Cuidados
 const btCuidados = document.querySelector("#btCuidados");
+const fecharCuidados = document.querySelector("#fecharCuidados");
 const cuidados = document.querySelector("#cuidados");
 
-btTranca.addEventListener("click", function(){
-    tranca.showModal();
-});
-
-btFtranca.addEventListener("click", function(){
-    tranca.close();
-});
-
-btLace.addEventListener("click", function(){
-    lace.showModal();
-});
-
-btFlace.addEventListener("click", function(){
-    lace.close();
-});
-
-btAplique.addEventListener("click", function(){
-    aplique.showModal();
-});
-
-btManutencao.addEventListener("click", function(){
-    manutencao.showModal();
-});
-
-btHidro.addEventListener("click", function(){
-    hidro.showModal();
-});
-
-btCuidados.addEventListener("click", function(){
-    cuidados.showModal();
-});
+btCuidados.addEventListener("click", () => cuidados.showModal());
+fecharCuidados.addEventListener("click", () => cuidados.close());
